4223d7161c810bd60b7245bbe990f1e0f4bc56d4
mona1isa <zhangyu_neu@sina.com>
2025-08-24 08:01:39 +0800

    feat: 验证码模块

--- controllers/system/captcha_controller.go
+++ controllers/system/captcha_controller.go
@@ -1,1 +1,18 @@
 package system
+
+import "github.com/gin-gonic/gin"
+
+// https://zh.mojotv.cn/go/refactor-base64-captcha
+
+type CaptchaController struct {
+}
+
+// GenerateCaptchaHandler 生成验证码
+func (c *CaptchaController) GenerateCaptchaHandler(ctx *gin.Context) {
+
+}
+
+// VerifyCaptchaHandler 验证验证码
+func (c *CaptchaController) VerifyCaptchaHandler(ctx *gin.Context) {
+
+}
