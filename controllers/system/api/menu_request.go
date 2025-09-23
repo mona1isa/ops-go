d10dd4cc616344039513d1e49780519453307f1b
mona1isa <zhangyu_neu@sina.com>
2025-09-23 13:45:59 +0800

    fix: 菜单回显问题

--- controllers/system/api/menu_request.go
+++ controllers/system/api/menu_request.go
@@ -2,23 +2,25 @@ package api
 
 // AddMenuRequest 添加菜单请求参数
 type AddMenuRequest struct {
-	Name      string `json:"name" validate:"required"` // 菜单名称
-	ParentId  int    `json:"parentId"`                 // 父菜单ID
-	OrderNum  int    `json:"orderNum"`                 // 排序
-	Path      string `json:"path"`                     // 路由地址
-	Component string `json:"component"`                // 组件路径
-	IsAffix   bool   `json:"isAffix"`                  // 是否固定 	(true 固定  false 不固定)
-	IsIframe  bool   `json:"isIframe"`                 // 是否为内嵌 	(true是 false否)
-	IsLink    bool   `json:"isLink"`                   // 是否为外链	(true是 false否)
-	KeepAlive bool   `json:"keepAlive"`                // 是否缓存	(true缓存 false不缓存)
-	Type      string `json:"type"`                     // 菜单类型 	(M目录 C菜单 F按钮)
-	IsHide    bool   `json:"isHide"`                   // 显示状态	(true显示 false隐藏)
-	Status    bool   `json:"status"`                   // 菜单状态	（true正常 false停用）
-	Url       string `json:"url"`                      // 外链地址
-	Perms     string `json:"perms"`                    // 权限标识
-	Icon      string `json:"icon"`                     // 菜单图标
-	CreateBy  string `json:"createBy"`
-	UpdateBy  string `json:"updateBy"`
+	Name          string `json:"name" validate:"required"` // 菜单名称
+	ParentId      int    `json:"parentId"`                 // 父菜单ID
+	OrderNum      int    `json:"orderNum"`                 // 排序
+	Path          string `json:"path"`                     // 路由地址
+	Component     string `json:"component"`                // 组件路径
+	IsAffix       bool   `json:"isAffix"`                  // 是否固定 	(true 固定  false 不固定)
+	IsIframe      bool   `json:"isIframe"`                 // 是否为内嵌 	(true是 false否)
+	IsLink        bool   `json:"isLink"`                   // 是否为外链	(true是 false否)
+	KeepAlive     bool   `json:"keepAlive"`                // 是否缓存	(true缓存 false不缓存)
+	Type          string `json:"type"`                     // 菜单类型 	(M目录 C菜单 F按钮)
+	IsHide        bool   `json:"isHide"`                   // 显示状态	(true显示 false隐藏)
+	Status        bool   `json:"status"`                   // 菜单状态	（true正常 false停用）
+	Url           string `json:"url"`                      // 外链地址
+	Perms         string `json:"perms"`                    // 权限标识
+	Icon          string `json:"icon"`                     // 菜单图标
+	RequestUrl    string `json:"requestUrl"`               // 请求地址
+	RequestMethod string `json:"requestMethod"`            // 请求方法
+	CreateBy      string `json:"createBy"`
+	UpdateBy      string `json:"updateBy"`
 }
 
 // MenuListRequest 菜单列表请求参数
@@ -46,7 +48,7 @@ type Meta struct {
 type MenuVo struct {
 	Id        int    `json:"id"`        // 菜单ID
 	Name      string `json:"name"`      // 菜单名称
-	ParentId  int    `json:"parent_id"` // 父菜单ID
+	ParentId  int    `json:"parentId"`  // 父菜单ID
 	OrderNum  int    `json:"orderNum"`  // 排序
 	Path      string `json:"path"`      // 路由地址
 	Component string `json:"component"` // 组件路径
@@ -63,17 +65,19 @@ type MenuVo struct {
 }
 
 type MenuTree struct {
-	Id        int         `json:"id"`                 // 菜单ID
-	Name      string      `json:"name"`               // 菜单名称
-	ParentId  int         `json:"parent_id"`          // 父菜单ID
-	OrderNum  int         `json:"orderNum"`           // 排序
-	Path      string      `json:"path"`               // 路由地址
-	Component string      `json:"component"`          // 组件路径
-	Type      string      `json:"type"`               // 菜单类型 (M目录 C菜单 F按钮)
-	Status    bool        `json:"status"`             // 菜单状态 (true 正常 false 停用)
-	Perms     string      `json:"perms"`              // 权限标识
-	Icon      string      `json:"icon"`               // 菜单图标
-	Url       string      `json:"url"`                // 外链地址
-	Meta      Meta        `json:"meta"`               // 菜单元数据
-	Children  []*MenuTree `json:"children,omitempty"` // 子菜单
+	Id            int         `json:"id"`                 // 菜单ID
+	Name          string      `json:"name"`               // 菜单名称
+	ParentId      int         `json:"parentId"`           // 父菜单ID
+	OrderNum      int         `json:"orderNum"`           // 排序
+	Path          string      `json:"path"`               // 路由地址
+	Component     string      `json:"component"`          // 组件路径
+	Type          string      `json:"type"`               // 菜单类型 (M目录 C菜单 F按钮)
+	Status        bool        `json:"status"`             // 菜单状态 (true 正常 false 停用)
+	Perms         string      `json:"perms"`              // 权限标识
+	Icon          string      `json:"icon"`               // 菜单图标
+	Url           string      `json:"url"`                // 外链地址
+	RequestMethod string      `json:"requestMethod"`      // 请求方法
+	RequestUrl    string      `json:"requestUrl"`         // 请求地址
+	Meta          Meta        `json:"meta"`               // 菜单元数据
+	Children      []*MenuTree `json:"children,omitempty"` // 子菜单
 }
--- models/sys_menu.go
+++ models/sys_menu.go
@@ -1,21 +1,23 @@
 package models
 
 type SysMenu struct {
-	Name      string `gorm:"type:varchar(64);not null;unique;comment:菜单名称" json:"name"`
-	ParentId  int    `gorm:"type:int(11);comment:父菜单ID" json:"parentId"`
-	OrderNum  int    `gorm:"type:int(4);comment:显示顺序" json:"orderNum"`
-	Path      string `gorm:"type:varchar(200);comment:路由地址" json:"path"`
-	Component string `gorm:"type:varchar(255);comment:组件路径" json:"component"`
-	IsAffix   bool   `gorm:"type:tinyint(1);comment:是否固定（1 是 0 否）" json:"isAffix"`
-	IsIframe  bool   `gorm:"type:tinyint(1);comment:是否为内链（1是 0否）" json:"isIframe"`
-	IsLink    bool   `gorm:"type:tinyint(1);comment:是否为外链（1是 0否）" json:"isLink"`
-	KeepAlive bool   `gorm:"type:tinyint(1);comment:是否缓存（true缓存 false不缓存）" json:"keepAlive"`
-	Type      string `gorm:"type:char(1);comment:菜单类型（M目录 C菜单 F按钮）" json:"type"`
-	IsHide    bool   `gorm:"type:tinyint(1);comment:是否隐藏（1隐藏 0显示）" json:"isHide"`
-	Status    bool   `gorm:"type:char(1);default:0;comment:菜单状态（1正常 0停用）" json:"status"`
-	Url       string `gorm:"type:varchar(128);default:null;comment:外链地址" json:"url"`
-	Perms     string `gorm:"type:varchar(100);comment:权限标识" json:"perms"`
-	Icon      string `gorm:"type:varchar(100);comment:菜单图标" json:"icon"`
+	Name          string `gorm:"type:varchar(64);not null;unique;comment:菜单名称" json:"name"`
+	ParentId      int    `gorm:"type:int(11);comment:父菜单ID" json:"parentId"`
+	OrderNum      int    `gorm:"type:int(4);comment:显示顺序" json:"orderNum"`
+	Path          string `gorm:"type:varchar(200);comment:路由地址" json:"path"`
+	Component     string `gorm:"type:varchar(255);comment:组件路径" json:"component"`
+	IsAffix       bool   `gorm:"type:tinyint(1);comment:是否固定（1 是 0 否）" json:"isAffix"`
+	IsIframe      bool   `gorm:"type:tinyint(1);comment:是否为内链（1是 0否）" json:"isIframe"`
+	IsLink        bool   `gorm:"type:tinyint(1);comment:是否为外链（1是 0否）" json:"isLink"`
+	KeepAlive     bool   `gorm:"type:tinyint(1);comment:是否缓存（true缓存 false不缓存）" json:"keepAlive"`
+	Type          string `gorm:"type:char(1);comment:菜单类型（M目录 C菜单 F按钮）" json:"type"`
+	IsHide        bool   `gorm:"type:tinyint(1);comment:是否隐藏（1隐藏 0显示）" json:"isHide"`
+	Status        bool   `gorm:"type:char(1);default:0;comment:菜单状态（1正常 0停用）" json:"status"`
+	Url           string `gorm:"type:varchar(128);default:null;comment:外链地址" json:"url"`
+	Perms         string `gorm:"type:varchar(100);comment:权限标识" json:"perms"`
+	Icon          string `gorm:"type:varchar(100);comment:菜单图标" json:"icon"`
+	RequestUrl    string `gorm:"type:varchar(100);comment:接口请求地址" json:"requestUrl"`
+	RequestMethod string `gorm:"type:varchar(10);comment:接口请求方法" json:"requestMethod"`
 	Base
 }
 
